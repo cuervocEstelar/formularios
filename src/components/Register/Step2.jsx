56f1cc7dd86aee3bf5bde5d05bebf9df6ad0817d
David Antonio Salas Pasten <david salas>
2025-07-15 18:10:44 -0400

    daddy yankee

--- src/components/Register/Step2.jsx
+++ src/components/Register/Step2.jsx
@@ -1,63 +1,81 @@
 const Step2 = ({ formData, onChange }) => {
   return (
     <>
-      <label className="form-label">Contraseña</label>
-      <input
-        className="form-input"
-        type="password"
-        name="password"
-        value={formData.password}
-        onChange={onChange}
-      />
+      {/* Grupo: Contraseña */}
+      <div className="inputs-box">
+        <label className="form-label">Contraseña</label>
+        <input
+          className="form-input"
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={onChange}
+        />
+      </div>
 
-      <label className="form-label">Verificación de contraseña</label>
-      <input
-        className="form-input"
-        type="password"
-        name="passwordVerification"
-        value={formData.passwordVerification}
-        onChange={onChange}
-      />
+      {/* Grupo: Verificación de contraseña */}
+      <div className="inputs-box">
+        <label className="form-label">Verificación de contraseña</label>
+        <input
+          className="form-input"
+          type="password"
+          name="passwordVerification"
+          value={formData.passwordVerification}
+          onChange={onChange}
+        />
+      </div>
 
-      <label className="form-label">Ciudad</label>
-      <input
-        className="form-input"
-        type="text"
-        name="city"
-        value={formData.city}
-        onChange={onChange}
-      />
+      {/* Grupo: Ciudad */}
+      <div className="inputs-box">
+        <label className="form-label">Ciudad</label>
+        <input
+          className="form-input"
+          type="text"
+          name="city"
+          value={formData.city}
+          onChange={onChange}
+        />
+      </div>
 
-      <label className="form-label">Moneda</label>
-      <input
-        className="form-input"
-        type="text"
-        name="currency"
-        value={formData.currency}
-        onChange={onChange}
-      />
+      {/* Grupo: Moneda */}
+      <div className="inputs-box">
+        <label className="form-label">Moneda</label>
+        <input
+          className="form-input"
+          type="text"
+          name="currency"
+          value={formData.currency}
+          onChange={onChange}
+        />
+      </div>
 
-      <label className="form-label">Código promocional</label>
-      <input
-        className="form-input"
-        type="text"
-        name="promoCode"
-        value={formData.promoCode}
-        onChange={onChange}
-      />
-<div className="terms-container">
-     <input
-        className="checkbox-terms"
-        type="checkbox"
-        name="termsAccepted"
-        checked={formData.termsAccepted}
-        onChange={onChange}
+      {/* Grupo: Código promocional */}
+      <div className="inputs-box">
+        <label className="form-label">Código promocional</label>
+        <input
+          className="form-input"
+          type="text"
+          name="promoCode"
+          value={formData.promoCode}
+          onChange={onChange}
+        />
+      </div>
 
-      />  <label className="form-label-terms">Estoy de acuerdo con todos los <b>Términos y condiciones y la Política de privacidad</b>  y soy mayor de 18 años.</label>
-     
-</div>
+      {/* Grupo: Términos y condiciones */}
+      <div className="inputs-box terms-container">
+        <input
+          className="checkbox-terms"
+          type="checkbox"
+          name="termsAccepted"
+          checked={formData.termsAccepted}
+          onChange={onChange}
+        />
+        <label className="form-label-terms">
+          Estoy de acuerdo con todos los <b>Términos y condiciones y la Política de privacidad</b> y soy mayor de 18 años.
+        </label>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Step2
+export default Step2;
